45910f34b43364b8835e552f06992a81ce52761c
Keshav Babbar <keshavbabbar007@gmail.com>
2025-04-08 16:19:24 +0530

    update

---
+++ Todo-list/script.js
@@ -0,0 +1,24 @@
+    let todolist = [];
+
+     renderTodoList();
+
+    function renderTodoList(){
+            let todoListElement = '';
+            for(let i = 0; i < todolist.length; i++){
+                let todo = todolist[i];
+                let html = `<p>${todo}</p>`;
+                todoListElement += html;
+    }
+
+    document.querySelector('.js-todo-list').innerHTML = todoListElement;
+    }
+
+
+    function addTodo(){
+        const inputElement= document.querySelector('.js-Todo-List');
+        const name = inputElement.value;
+        todolist.push(name);
+        console.log(todolist);
+
+        renderTodoList();
+    }
