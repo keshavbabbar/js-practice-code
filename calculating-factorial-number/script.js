dcbfb2611379a017cddfb56419fe25368ed656c8
Keshav Babbar <keshavbabbar007@gmail.com>
2025-05-04 22:30:17 +0530

    update

---
+++ calculating-factorial-number/script.js
@@ -0,0 +1,8 @@
+    let a = 6
+
+    function factorial(number){
+        let arr = Array.from(Array(number + 1).keys())
+        console.log(arr)
+    }
+
+    factorial(a)
---
+++ events/script.js
@@ -0,0 +1,5 @@
+let btn = document.getElementById("btn");
+
+btn.addEventListener("dblclick", () => {
+   document.querySelector(".box ").innerHTML = "hy my name is keshav"
+})
